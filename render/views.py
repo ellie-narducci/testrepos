622f22ff95a07f665e521a6389c75fa07cceb0b4
ellie-narducci <ellie@exponentlabs.io>
2021-02-09 13:30:14 +0800

    test flicker site up and down

--- render/views.py
+++ render/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+lapTime = 0
 
 def index(request):
-   #return render(request, 'render/index.html', {})
-   return HttpResponse(status=500)
+    lapTime = datetime.time.minute
+    previouslyUp = True
+    while True:
+        currentTime = datetime.time.minute
+        if (currentTime >= lapTime+2):
+            if (previouslyUp):
+                previouslyUp = False
+                lapTime = datetime.time.minute
+                return HttpResponse(status=500)
+            elif (not previouslyUp):
+                previouslyUp = True
+                lapTime = datetime.time.minute
+                return render(request, 'render/index.html', {})
