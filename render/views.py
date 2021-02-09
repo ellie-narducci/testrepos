9267278cd8dcc47d01dd8d4028817b1b9e12c6a4
ellie-narducci <ellie@exponentlabs.io>
2021-02-09 13:53:46 +0800

    test 400

--- render/views.py
+++ render/views.py
@@ -8,14 +8,7 @@ lapTime = 0
 previouslyUp = False
 
 def index(request):
-    return render(request, 'render/index.html', {})
-    # currentTime = datetime.time.minute
-    # if (currentTime >= lapTime+2):
-    #     if (previouslyUp):
-    #         previouslyUp = False
-    #         lapTime = datetime.time.minute
-    #         return HttpResponse(status=400)
-    #     elif (not previouslyUp):
-    #         previouslyUp = True
-    #         lapTime = datetime.time.minute
-    #         return render(request, 'render/index.html', {})
+    currentTime = datetime.time.minute
+    print(currentTime)
+    # return render(request, 'render/index.html', {})
+    return HttpResponse(status=400)
